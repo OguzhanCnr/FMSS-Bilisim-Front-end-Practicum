5c3e3c19d084ed81fe7dec49fa89e77a914db23d
Oguzhan <ogzhn.cnr0@gmail.com>
2023-03-26 17:31:44 +0300

    Add commit

--- Week1/app.js
+++ Week1/app.js
@@ -1,14 +1,18 @@
 import axios from 'axios';
 
 async function getUserData(userId) {
+  //user ve postlar Api den çekildi
     const userResponse = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
     const postResponse = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
+    //user Data oluşturuldu ve posts kısmına kullanıcının postları getirildi.
     const userData = userResponse.data;
     userData.posts = postResponse.data;
     return userData;
   }
   
+  //userId si 1 olan user çağırıldı
   const userData = await getUserData(1);
   
+  //Ekrana log ile bastırıldı
   console.log(userData);
   
